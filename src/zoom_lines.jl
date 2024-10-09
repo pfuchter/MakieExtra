bf0db58dd606770188ba8d827893f6428861e8e5
pfuchter <p.fuchter@bristol.ac.uk>
2024-10-09 12:32:19 +0100

    works for axis inside axis

--- src/zoom_lines.jl
+++ src/zoom_lines.jl
@@ -11,15 +11,49 @@ function zoom_lines!(ax1, ax2; strokewidth=1.5, strokecolor=:black, color=(:blac
 			(vp=vps[2], lim=lims[2]),
 			(vp=vps[1], lim=lims[1]),
 		) : nothing
-		fs = isnothing(axs) ? nothing : if right(axs[1].vp) < left(axs[2].vp)
-			(topright, bottomright, topleft, bottomleft)
-		elseif left(axs[1].vp) > right(axs[2].vp)
-			(topleft, bottomleft, topright, bottomright)
-		elseif bottom(axs[1].vp) < top(axs[2].vp)
-			(topleft, topright, bottomleft, bottomright)
-		elseif bottom(axs[1].vp) > top(axs[2].vp)
-			(bottomleft, bottomright, topleft, topright)
+
+		
+		#check if either ax is completely inside the other:
+		ax1insideax2 = vps[1] ⊆ vps[2]
+		ax2insideax1 = vps[2] ⊆ vps[1]
+		if isnothing(axs)
+			fs = nothing
+		else
+		if ax2insideax1 || ax1insideax2
+			Ax1 = ax2insideax1 ? axs[2] : axs[1]
+			Ax2 = ax2insideax1 ? axs[1] : axs[2]
+			Ax1 = axs[2]
+			Ax2 = axs[1]
+			rect_bottomleft = shift_range(bottomleft(Ax2.lim), Ax1.lim => Ax1.vp)
+			rect_topright   = shift_range(topright(Ax2.lim), Ax1.lim => Ax1.vp)
+			
+			rect_left = rect_bottomleft[1]
+			rect_right = rect_topright[1]
+			rect_top = rect_topright[2]
+			rect_bottom = rect_bottomleft[2]
+
+			fs = isnothing(axs) ? nothing : if rect_right < left(Ax2.vp)
+				(topleft, bottomleft, topright, bottomright)
+			elseif rect_left > right(Ax2.vp)
+				(topright, bottomright, topleft, bottomleft)
+			elseif rect_top < bottom(Ax2.vp)
+				(bottomleft, bottomright, topleft, topright)
+			elseif rect_left < right(Ax2.vp)
+				(topleft, topright, bottomleft, bottomright)
+			end
+		else
+			fs = isnothing(axs) ? nothing : if right(axs[1].vp) < left(axs[2].vp)
+				(topright, bottomright, topleft, bottomleft)
+			elseif left(axs[1].vp) > right(axs[2].vp)
+				(topleft, bottomleft, topright, bottomright)
+			elseif bottom(axs[1].vp) > top(axs[2].vp)
+				(bottomleft, bottomright, topleft, topright)
+			elseif bottom(axs[1].vp) < top(axs[2].vp)
+				(topleft, topright, bottomleft, bottomright)
+			end
 		end
+	end
+		# display(lims)
 		(
 			rect1=$(ax2.finallimits),
 			rect2=$(ax1.finallimits),
